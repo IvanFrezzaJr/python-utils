7dcd044a4579b276bafa47d799515e46329e62d6
IvanFrezzaJr <ivanfrezza@gmail.com>
2021-08-08 16:41:44 +0100

    wip

--- flask-boilerplate.py
+++ flask-boilerplate.py
@@ -70,7 +70,7 @@ def create_boilerplate(path_project):
     # database files
     _create_file(os.path.join(database_path, "__init__.py"), _write_ext_database_file())
     _create_file(
-        os.path.join(database_path, "cli.py"), _write_ext_database_command_file(name)
+        os.path.join(database_path, "command.py"), _write_ext_database_command_file(name)
     )
     # database models folder
     models_path = os.path.join(database_path, "models")
@@ -359,6 +359,108 @@ def populate_db():
     )
 
 
+def __write_database_base_file(name):
+    return (
+        """
+from """+ name +""".ext.database import db
+from flask import abort
+from datetime import datetime
+
+class Base(db.Model):
+    __abstract__ = True
+
+    id = db.Column(db.Integer(), primary_key=True)
+    created_at = db.Column(
+        db.DateTime(),
+        nullable=False,
+        default=datetime.now,
+        server_default=db.func.now())
+    updated_at = db.Column(db.DateTime(),
+        nullable=False,
+        default=datetime.now,
+        server_default=db.func.now(),
+        server_onupdate=db.func.now())
+
+
+    @classmethod
+    def create(model, data):
+        item = model(**data)
+        try:
+            
+            db.session.add(item)
+            db.session.commit()
+            return item
+        except Exception as e:
+            db.session.rollback()
+
+
+    @classmethod
+    def delete_by_id(model, id):
+        item = model.query.get(id) or abort(404, "item not found")
+        try:
+            db.session.delete(item)
+            db.session.commit()
+            return {}, 204
+        except Exception as e:
+            db.session.rollback()
+
+
+
+    @classmethod
+    def update_by_id(model, id, data):
+        try:
+            query = model.query.filter(model.id == id)
+            if data:
+                query.update(data)
+                db.session.commit()
+                item = query.first()            
+                return item
+        except Exception as e:
+            db.session.rollback()
+
+
+
+    @classmethod
+    def get_by_id(model, id):
+        data = model.query.get(id) or abort(404, "item not found")
+        return data
+
+
+    @classmethod
+    def get_all(model):
+        return model.query.all()
+
+    @classmethod
+    def get_list(model, args):
+        args = dict(args)
+        
+        query = model.filters(args, model.query)
+        data = query.all()
+        return {
+            "data": data,
+            "meta": {}
+        }, 200
+
+    @classmethod
+    def filters(model, args, query):
+
+        filter_args = args.copy()
+
+        if len(filter_args) < 1:
+            return query
+
+        filters = []
+        for arg in args:
+            attr = model.get_attr(arg)
+            value = args[arg]
+            filters.append(attr == value)
+
+        return query.filter(*filters)
+
+
+        """
+    )
+
 def __write_model_user_file(name):
     return (
         """
@@ -417,9 +519,9 @@ def init_app(app):
 def _write_ext_database_file():
     return """
   
-from flask_sqlalchemy import SqlAlchemy
+from flask_sqlalchemy import SQLAlchemy
 
-db = SqlAlchemy()
+db = SQLAlchemy()
 
 
 def init_app(app):
